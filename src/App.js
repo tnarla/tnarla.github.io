fd73dbe8ed638912586d8a438ddad82d0f808303
Tru Narla <trushithanarla@gmail.com>
2019-09-10 13:59:02 -0700

    add bg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { WaveBG } from "./svgs/bg";
 
 function App() {
-  return <div className="App">HELLO</div>;
+  return (
+    <div className="App">
+      <WaveBG />
+    </div>
+  );
 }
 
 export default App;
