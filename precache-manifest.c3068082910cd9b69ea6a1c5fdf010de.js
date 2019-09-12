065089381bd6c5271884192c298ad3252e00c6b4
Tru Narla <trushithanarla@gmail.com>
2019-09-11 18:05:57 -0700

    Updates

---
+++ precache-manifest.c3068082910cd9b69ea6a1c5fdf010de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d1168ffdad4e52d30a7af0fd7866d0",
+    "url": "/trunarla/index.html"
+  },
+  {
+    "revision": "878ed99e842e4c746858",
+    "url": "/trunarla/static/css/main.8c509593.chunk.css"
+  },
+  {
+    "revision": "4ccc692442f3cb7aae10",
+    "url": "/trunarla/static/js/2.4d97f0e1.chunk.js"
+  },
+  {
+    "revision": "878ed99e842e4c746858",
+    "url": "/trunarla/static/js/main.91068eff.chunk.js"
+  },
+  {
+    "revision": "76380c2288643624d5ec",
+    "url": "/trunarla/static/js/runtime~main.0af86ecf.js"
+  }
+]);
